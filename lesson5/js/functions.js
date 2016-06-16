db4379151a02d057f76bd5e62709d7f792eb02b7
Kalter <kalterxad34@gmail.com>
2016-06-16 22:08:06 +0300

    getsixteen

--- lesson5/js/functions.js
+++ lesson5/js/functions.js
@@ -15,6 +15,21 @@ function showFunc(i) {
     document.getElementById(i).style.display = "block";
 }
 
+function getSixteen(a) {
+    var i = 0, b = [], c = [];
+    while (a > 0){
+       b[i] = a % 16;
+       switch (b[i]){
+           case 10: b[i] = 'A'; break;
+           case 11: b[i] = 'B'; break;
+           case 12: b[i] = 'C'; break;
+           case 13: b[i] = 'D'; break;
+           case 14: b[i] = 'E'; break;
+           case 15: b[i] = 'F';
+       }
+       c[i] = Math.ceil(a / 16);
+    }
+}
 
 function getColor() {
     var str = '';
